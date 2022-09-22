8c13292ff43b42a0e5159752431e6a4c5ab08c74
Abha Himani <abhahimani2002@gmail.com>
2022-09-22 14:51:24 +0530

    up

--- Dynamic Programming/p.cpp
+++ Dynamic Programming/p.cpp
@@ -1,36 +1,54 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-long findKthLargest(vector<int> &arr, int k)
+int findSplitPoint(vector<int> skill)
 {
+    int n = skill.size();
+    int leftSum = 0;
 
-    priority_queue<int, vector<int>, greater<int>> minH;
-    for (int i = 0; i < arr.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        minH.push(arr[i]);
-        if (minH.size() > k)
+
+        leftSum += arr[i];
+
+        int rightSum = 0;
+        for (int j = i + 1; j < n; j++)
+            rightSum += arr[j];
+
+        if (leftSum == rightSum)
+            return i + 1;
+    }
+
+    return -1;
+}
+
+// Prints two parts after finding split point using
+// findSplitPoint()
+long printTwoParts(vector<int> skill)
+{
+    int n = skill.size();
+    int splitPoint = findSplitPoint(skill, n);
+    int left = 1;
+    int right = 1;
+    long result = 0;
+    if (splitPoint == -1 || splitPoint == n)
+    {
+
+        return -1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (splitPoint >= i)
         {
-            minH.pop();
+            right *= skill[i];
+        }
+        else
+        {
+            left *= skill[i];
         }
     }
-    return minH.top();
+    return left + right;
 }
-
-long getMinimumHealth(vector<int> ini_players, vector<int> new_players, int n)
-
-{
-
-    long result = 0;
-    result += findKthLargest(ini_players, n);
-
-    for (int i = 0; i < new_players.size(); i++)
-    {
-        ini_players.push_back(new_players[i]);
-        result += findKthLargest(ini_players, n);
-    }
-}
-
 int main()
 {
+    vector<int> s = {1, 2, 3, 2};
+    printTwoParts(s);
 }
